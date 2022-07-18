7b13405a3ee310aea9a664cbdafea4489cceecde
Remrem1645 <dyan@cpp.edu>
2022-07-17 22:54:46 -0700

    contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import '../../App.css'
+import './Contact.css'
+import {
+    Animator,
+    batch,
+    Fade,
+    ScrollPage,
+    ScrollContainer,
+    Sticky,
+    MoveOut
+} from 'react-scroll-motion';
 
 function Contact() {
-    return(
-        <h1 className='Contact'>
-            Contact
-        </h1>
-
+    return (
+        <div className='contact-page'>
+            <ScrollContainer>
+                <ScrollPage page={0}>
+                    <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                        <h1 className='contact-page-title'><b>Contact Us</b></h1>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage page={1}>
+                    <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                        <h1 className='contact-page-title'><b>Phone Number</b></h1>
+                        <p className='contact-page-phonenum'>626-425-1150</p>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage page={2}>
+                    <Animator animation={batch(Fade(), Sticky())}>
+                        <h1 className='contact-page-title'><b>Hours Of Operation</b></h1>
+                        <p className='contact-page-hours'>
+                            Mon: 10:00 AM - 6:00 PM <br></br>
+                            Tue: 10:00 AM - 6:00 PM <br></br>
+                            Wed: 10:00 AM - 6:00 PM <br></br>
+                            Thu: 10:00 AM - 6:00 PM <br></br>
+                            Fri: 10:00 AM - 6:00 PM <br></br>
+                            Sat: 10:00 AM - 6:00 PM <br></br>
+                            Sun: 10:00 AM - 6:00 PM <br></br>
+                        </p>
+                    </Animator>
+                </ScrollPage>
+            </ScrollContainer>
+        </div>
     )
 }
 
