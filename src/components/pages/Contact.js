f1f62445ee821e7f40eba4ce8e358106bf5fdf24
Remrem1645 <dyan@cpp.edu>
2022-07-17 23:30:59 -0700

    fixes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,7 +8,7 @@ import {
     ScrollPage,
     ScrollContainer,
     Sticky,
-    MoveOut
+    Move
 } from 'react-scroll-motion';
 
 function Contact() {
@@ -16,18 +16,13 @@ function Contact() {
         <div className='contact-page'>
             <ScrollContainer>
                 <ScrollPage page={0}>
-                    <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                    <Animator animation={batch(Fade(), Sticky(), Move())}>
                         <h1 className='contact-page-title'><b>Contact Us</b></h1>
+                        <p className='contact-page-phonenum'>(626) 866-3402</p>
                     </Animator>
                 </ScrollPage>
                 <ScrollPage page={1}>
-                    <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-                        <h1 className='contact-page-title'><b>Phone Number</b></h1>
-                        <p className='contact-page-phonenum'>626-425-1150</p>
-                    </Animator>
-                </ScrollPage>
-                <ScrollPage page={2}>
-                    <Animator animation={batch(Fade(), Sticky())}>
+                    <Animator animation={batch(Move(), Sticky(), Fade())}>
                         <h1 className='contact-page-title'><b>Hours Of Operation</b></h1>
                         <p className='contact-page-hours'>
                             Mon: 10:00 AM - 6:00 PM <br></br>
